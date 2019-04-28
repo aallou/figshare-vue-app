657eafbe26d1ea3f92451dfb61c3df9fa2c7fd85
Abdelhafid Allou <allou.abdelhafid@gmail.com>
2019-04-28 14:02:11 +0200

    Add Figshare service + json server data mock

---
+++ src/components/Articles.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <div v-for="article in articles" :key="article.id">
+      <h3>{{ article.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import FigShareService from "@/services/FigShareService";
+
+export default {
+  data() {
+    return {
+      message: "Hello word",
+      articles: []
+    };
+  },
+  mounted() {
+    FigShareService.getArticles()
+      .then(response => {
+        this.articles = response.data;
+      })
+      .catch(error => console.log(error));
+  }
+};
+</script>
+
+<style></style>
