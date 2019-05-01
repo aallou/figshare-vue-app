cabe76bb574663cda1248b229542e9738c256c57
Abdelhafid Allou <allou.abdelhafid@gmail.com>
2019-05-01 10:44:21 +0200

    Add menu. items

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <v-toolbar>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>Title</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
-        <v-btn flat>Link Two</v-btn>
-        <v-btn flat>Link Three</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-    </div>
-    <router-view/>
+    <v-container>
+      <v-toolbar>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>FigShare Dashbord</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>
+            <router-link to="/">Home</router-link>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <router-view />
+    </v-container>
   </div>
 </template>
 
